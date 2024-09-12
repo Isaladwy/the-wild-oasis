52a3350bff189751d8d09fb29b60f0e5016ff6c2
Isaladwy <isaladwy@outlook.com>
2024-09-12 18:47:44 +0300

    Fetching a cabin data

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,12 +1,12 @@
 import supabase from './supabase';
 
-export async function getCabins() {
+export default async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
 
   if (error) {
     console.error(error);
     throw new Error("Cabins couldn't be loaded");
-  } 
+  }
 
   return data;
 }
